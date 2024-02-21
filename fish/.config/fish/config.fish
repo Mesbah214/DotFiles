e77eef235d55ba8b3cabfe7c51c9b590c273defa
Mesbah Uddin <msbh214@gmail.com>
2024-02-22 02:03:42 +0600

    added i3-wm config & updated basic movement

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,9 @@ alias l='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
+
 fish_vi_key_bindings
 bind --mode insert --sets-mode default jk repaint
+
 zoxide init fish | source
 end
